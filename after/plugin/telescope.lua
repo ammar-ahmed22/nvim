e51cd4f69c9be268a7aebd4103ec4c16946d1624
Ammar Ahmed <ammar.ahmed1@uwaterloo.ca>
2024-11-29 02:46:09 -0500

    feat: remap for file search

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,4 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-f>', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>ff', builtin.git_files, { desc = 'Telescope find files tracked by git' })
+vim.keymap.set('n', '<C-space>', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fs', builtin.grep_string, { desc = "Telescope find files with word"})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Telescope search with grep"})
