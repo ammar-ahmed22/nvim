b18c3f1be7e0f1826d5f5cf983ec92b0f3ec6aee
Ammar Ahmed <ammar.ahmed@withmantle.com>
2025-01-23 11:54:56 -0500

    feat: code action fix all

--- lua/ammar/keymaps.lua
+++ lua/ammar/keymaps.lua
@@ -1,25 +1,25 @@
--- Open and focus the file tree 
+-- Open and focus the file tree
 vim.keymap.set("n", "<leader>f", ":Neotree focus toggle<CR>", { desc = "Toggle [F]ile tree", silent = true })
--- Quit window 
+-- Quit window
 vim.keymap.set("n", "<leader>q", "<C-w>q", { desc = "[Q]uit the focused window", silent = true })
 
 -- Escape in normal mode to remove search higlight
 vim.keymap.set("n", '<Esc>', '<cmd>nohlsearch<CR>')
 
--- Git add and commit 
-vim.keymap.set("n", "<leader>gac", ":AddCommit<CR>", { desc = "[G]it [A]dd and [C]ommit"})
+-- Git add and commit
+vim.keymap.set("n", "<leader>gac", ":AddCommit<CR>", { desc = "[G]it [A]dd and [C]ommit" })
 
 -- Git push
 vim.keymap.set("n", "<leader>gp", ":Git push<CR>", { noremap = true, desc = "Git [P]ush" })
 
--- Git status 
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Git [S]tatus"})
+-- Git status
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Git [S]tatus" })
 
 vim.keymap.set("n", "<leader>gb", ":Git branch <CR>", { desc = "Git [B]ranch" })
 
 -- Window navigation
 vim.keymap.set('n', '<leader>w', '<C-w>w', { desc = "Switch [W]indow (R-L)" })
-vim.keymap.set('n', '<leader>W', '<C-w>W', { desc = "Switch [W]indow (L-R)"})
+vim.keymap.set('n', '<leader>W', '<C-w>W', { desc = "Switch [W]indow (L-R)" })
 vim.keymap.set('n', '<leader>r', '<C-w>l', { desc = "Switch to the right window" })
 vim.keymap.set('n', '<leader>e', '<C-w>h', { desc = "Switch to the left window" })
 
@@ -27,30 +27,32 @@ vim.keymap.set('n', '<leader>e', '<C-w>h', { desc = "Switch to the left window" 
 vim.keymap.set('n', '<leader>v', ':vsplit<CR>', { desc = "New [V]ertical Window" })
 
 
--- Page up and down center cursor 
+-- Page up and down center cursor
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
--- Closing terminal in insert mode 
+-- Closing terminal in insert mode
 vim.keymap.set('t', "<Esc>", '<C-\\><C-n>:q<CR>', { noremap = true, silent = true })
 vim.keymap.set('t', "<C-\\>", '<C-\\><C-n>:q<CR>', { noremap = true, silent = true })
 
--- Opening terminal session in horizontal split window 
-vim.keymap.set('n', '<leader>t', function ()
+-- Opening terminal session in horizontal split window
+vim.keymap.set('n', '<leader>t', function()
     vim.cmd("split | terminal")
-end, { noremap = true, silent = true, desc = "Start [T]erminal"})
+end, { noremap = true, silent = true, desc = "Start [T]erminal" })
 
 
--- ChatGPT plugin keybinding 
-vim.keymap.set('n', "<leader>cc", ":ChatGPT<CR>", { noremap = true, silent = true, desc = "Open [C]hatGPT [C]hat"})
+-- ChatGPT plugin keybinding
+vim.keymap.set('n', "<leader>cc", ":ChatGPT<CR>", { noremap = true, silent = true, desc = "Open [C]hatGPT [C]hat" })
 
--- Alpha as home 
-vim.keymap.set('n', "<leader>h", ":Alpha<CR>", { noremap = true, silent = true, desc = "Return to [H]ome screen"})
+-- Alpha as home
+vim.keymap.set('n', "<leader>h", ":Alpha<CR>", { noremap = true, silent = true, desc = "Return to [H]ome screen" })
 
--- Newline under current line in normal mode 
-vim.keymap.set('n', "<leader>nl", "o<C-c>", { noremap = true, silent = true, desc = "[N]ew [L]ine under current"})
+-- Newline under current line in normal mode
+vim.keymap.set('n', "<leader>nl", "o<C-c>", { noremap = true, silent = true, desc = "[N]ew [L]ine under current" })
 
-vim.keymap.set('n', "gfr", ":%s/", { noremap = true, desc = "[F]ind and replace in file."})
-vim.keymap.set('n', 'gff', "/", { noremap = true, desc = "[F]ind in file."})
+vim.keymap.set('n', "gfr", ":%s/", { noremap = true, desc = "[F]ind and replace in file." })
+vim.keymap.set('n', 'gff', "/", { noremap = true, desc = "[F]ind in file." })
 
-vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, { desc = "[F]ormat the current buffer"})
+vim.keymap.set('n', '<leader>gf', function()
+    vim.lsp.buf.format({ timeout_ms = 10000 })
+end, { desc = "[F]ormat the current buffer" })
