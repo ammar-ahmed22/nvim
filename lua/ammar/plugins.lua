dbdafe8f4c8ef32b2d45257ff2b98e664389987a
Ammar Ahmed <ammar.ahmed@withmantle.com>
2025-01-27 14:39:01 -0500

    feat(plugins): neoscroll + feat(settings): shiftwidth + treesitter indent

--- lua/ammar/plugins.lua
+++ lua/ammar/plugins.lua
@@ -137,4 +137,10 @@ return require('packer').startup(function(use)
           "nvimtools/none-ls-extras.nvim"
       }
   }
+  use {
+      "karb94/neoscroll.nvim",
+      config = function ()
+        require("neoscroll").setup()
+      end
+  }
 end)
--- lua/ammar/settings.lua
+++ lua/ammar/settings.lua
@@ -25,8 +25,9 @@ vim.opt.relativenumber = true;
 -- Tabs and shift
 vim.opt.tabstop = 4;
 vim.opt.softtabstop = 4;
-vim.opt.shiftwidth = 4;
+vim.opt.shiftwidth = 2;
 vim.opt.expandtab = true;
+vim.opt.autoindent = true;
 vim.opt.smartindent = true;
 vim.opt.breakindent = true;
 
