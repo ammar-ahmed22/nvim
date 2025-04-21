38fbe1b7ec1d3612fcf0004d76180e535272858c
Ammar Ahmed <ammar.ahmed@withmantle.com>
2025-04-21 13:16:48 -0400

    feat(tree): Follow current file

--- lua/ammar/pluginconfigs/tree.lua
+++ lua/ammar/pluginconfigs/tree.lua
@@ -1,9 +1,12 @@
 require("neo-tree").setup({
-    enable_git_status = true,
-    enable_diagnostics = true,
-    filesystem = {
-        filtered_items = {
-            visible = true
-        }
+  enable_git_status = true,
+  enable_diagnostics = true,
+  filesystem = {
+    filtered_items = {
+      visible = true
+    },
+    follow_current_file = {
+      enabled = true
     }
+  }
 })
