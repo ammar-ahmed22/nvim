109264fef52825d51bc17d3a193bb89b17467d26
Ammar Ahmed <ammar.ahmed1@uwaterloo.ca>
2025-05-01 16:30:49 -0400

    fix: barbar tabline transparent bg

--- lua/ammar/pluginconfigs/colorscheme.lua
+++ lua/ammar/pluginconfigs/colorscheme.lua
@@ -4,5 +4,4 @@ require("synthweave").setup({})
 vim.opt.background = "dark"
 vim.cmd.colorscheme("catppuccin-mocha")
 vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ffd700", bold = true })
-
-
+vim.api.nvim_set_hl(0, "BufferTabpageFill", { bg = "none" })
