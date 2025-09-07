6f086bff5b2dc7a8b372f6d432e3530432343963
Ammar Ahmed <ammar.ahmed2203@gmail.com>
2025-09-07 18:58:30 -0400

    feat: add opencode

---
+++ lua/ammar/pluginconfigs/opencode.lua
@@ -0,0 +1,19 @@
+vim.g.opencode_opts = {}
+
+vim.opt.autoread = true
+vim.keymap.set('n', '<leader>ot', function() require('opencode').toggle() end, { desc = "Toggle Opencode" })
+vim.keymap.set('n', '<leader>oA', function() require('opencode').ask() end, { desc = "Ask opencode" })
+vim.keymap.set('n', '<leader>oa', function() require('opencode').ask('@cursor: ') end,
+  { desc = "Ask opencode about this" })
+vim.keymap.set('v', '<leader>oa', function() require('opencode').ask('@selection: ') end,
+  { desc = "Ask opencode about selection" })
+vim.keymap.set('v', '<leader>on', function() require('opencode').command('session_new') end,
+  { desc = "New opencode session" })
+vim.keymap.set('n', '<leader>oy', function() require('opencode').command('messages_copy') end,
+  { desc = "Copy last opencode response" })
+vim.keymap.set('n', '<S-C-u>', function() require('opencode').command('messages_half_page_up') end,
+  { desc = 'Messages half page up' })
+vim.keymap.set('n', '<S-C-d>', function() require('opencode').command('messages_half_page_down') end,
+  { desc = 'Messages half page down' })
+vim.keymap.set({ 'n', 'v' }, '<leader>os', function() require('opencode').select() end,
+  { desc = 'Select opencode prompt' })
