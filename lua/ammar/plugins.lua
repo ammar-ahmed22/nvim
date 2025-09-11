ba6224c72c92ad30a10393f2d594b2ba08721347
Ammar Ahmed <ammar.ahmed2203@gmail.com>
2025-09-11 16:03:11 -0400

    feat: wip for jupyter

--- lua/ammar/plugins.lua
+++ lua/ammar/plugins.lua
@@ -183,26 +183,27 @@ return require('packer').startup(function(use)
     requires = {
       'folke/snacks.nvim', config = function() require('snacks').setup({ input = { enabled = true } }) end,
     },
-    -- config = function()
-    --   vim.g.opencode_opts = {}
-    --
-    --   vim.opt.autoread = true
-    --   vim.keymap.set('n', '<leader>ot', function() require('opencode').toggle() end, { desc = "Toggle Opencode" })
-    --   vim.keymap.set('n', '<leader>oA', function() require('opencode').ask() end, { desc = "Ask opencode" })
-    --   vim.keymap.set('n', '<leader>oa', function() require('opencode').ask('@cursor: ') end,
-    --     { desc = "Ask opencode about this" })
-    --   vim.keymap.set('v', '<leader>oa', function() require('opencode').ask('@selection: ') end,
-    --     { desc = "Ask opencode about selection" })
-    --   vim.keymap.set('v', '<leader>on', function() require('opencode').command('session_new') end,
-    --     { desc = "New opencode session" })
-    --   vim.keymap.set('v', '<leader>oy', function() require('opencode').command('messages_copy') end,
-    --     { desc = "Copy last opencode response" })
-    --   vim.keymap.set('n', '<S-C-u>', function() require('opencode').command('messages_half_page_up') end,
-    --     { desc = 'Messages half page up' })
-    --   vim.keymap.set('n', '<S-C-d>', function() require('opencode').command('messages_half_page_down') end,
-    --     { desc = 'Messages half page down' })
-    --   vim.keymap.set({ 'n', 'v' }, '<leader>os', function() require('opencode').select() end,
-    --     { desc = 'Select opencode prompt' })
-    -- end
+  }
+  use {
+    'goerz/jupytext.nvim',
+    tag = 'v0.2.0',
+    config = function()
+      require('jupytext').setup({})
+    end,
+  }
+  use {
+    "benlubas/molten-nvim",
+    run = ":UpdateRemotePlugins",
+    config = function()
+      -- set globals (examples; all are optional)
+      vim.g.molten_image_provider = "none"-- or "wezterm" or "none"
+      vim.g.molten_auto_open_output = true
+
+      -- handy keymaps
+      vim.keymap.set("n", "<localleader>mi", ":MoltenInit python3<CR>", { silent = true })
+      vim.keymap.set("n", "<localleader>rl", ":MoltenEvaluateLine<CR>", { silent = true })
+      vim.keymap.set("v", "<localleader>rv", ":<C-u>MoltenEvaluateVisual<CR>gv", { silent = true })
+      vim.keymap.set("n", "<localleader>oo", ":noautocmd MoltenEnterOutput<CR>", { silent = true })
+    end,
   }
 end)
